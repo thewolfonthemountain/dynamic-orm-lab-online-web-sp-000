cad144db075cd58138fc8b870501d81e3f75d357
Sean Langton <slangton1@gmail.com>
2019-01-09 04:50:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,8 +19,7 @@ class InteractiveRecord
     table_info.each do |column|
       column_names << column["name"]
     end
-    column_names.compact
-    binding.pry
+    column_names
   end
 
   self.column_names.each do |col_name|
