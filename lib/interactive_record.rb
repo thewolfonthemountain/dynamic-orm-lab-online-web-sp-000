68e80a0bf0f9f661fff6f8ae50377c626b324429
Sean Langton <slangton1@gmail.com>
2019-01-09 04:48:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,7 +14,7 @@ class InteractiveRecord
     sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    binding.pruy
+    binding.pry
     column_names = []
 
     table_info.each do |column|
