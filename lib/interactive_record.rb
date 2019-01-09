6faec9b41fb8adee57944bfdab7d4c8e28e33fed
Sean Langton <slangton1@gmail.com>
2019-01-09 05:00:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,7 +33,7 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
 end
