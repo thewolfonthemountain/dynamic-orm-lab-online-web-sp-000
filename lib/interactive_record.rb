a5e70674627ff28b0f90862fb47e071f357b2985
Sean Langton <slangton1@gmail.com>
2019-01-09 04:57:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,10 +22,6 @@ class InteractiveRecord
     column_names.compact
   end
 
-  self.column_names.each do |col_name|
-    binding.pry
-    attr_accessor col_name.to_sym
-  end
 
   def initialize(options={})
     options.each do |property, value|
