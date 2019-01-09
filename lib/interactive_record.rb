54df38cd5db5669934e61714590b2a0902aebcb3
Sean Langton <slangton1@gmail.com>
2019-01-09 04:50:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  attr_accessor :id
+  attr_accessor :id, name
 
   def self.table_name
     self.to_s.downcase.pluralize
